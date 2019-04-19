79e47a4290efb74f13b67dc6623f39ea775d475d
dantearrighi <dantearrighi@gmail.com>
2019-04-19 13:11:25 -0300

    Errores de rutas
    Por no haber hecho las vistas tal como lo indicaba el curso, hay errores (creo que en las rutas) al añadir una nueva lista.

--- 05-deseos/src/app/services/deseos.service.ts
+++ 05-deseos/src/app/services/deseos.service.ts
@@ -11,12 +11,38 @@ export class DeseosService {
 
   constructor( ) {
    
-    const lista1 = new Lista('Asignación Universal por Hijo');
-    const lista2 = new Lista('SUAF - Salario');
+    this.cargarStorage();
+    
+   }
 
-    this.listas.push(lista1,lista2);
+   crearLista(titulo:string){
 
     
+    const nuevaLista = new Lista( titulo );
+    this.listas.push ( nuevaLista );
+    this.guardarStorage();
+
+    return nuevaLista.id;
 
    }
+
+  
+   guardarStorage(){
+
+      localStorage.setItem('data', JSON.stringify(this.listas) );
+
+   }
+
+   cargarStorage(){
+
+    if (localStorage.getItem('data') ){
+    this.listas = JSON.parse( localStorage.getItem('data') );
+    } else{
+      this.listas = [];
+    }
+
+   }
+
+
+
 }
