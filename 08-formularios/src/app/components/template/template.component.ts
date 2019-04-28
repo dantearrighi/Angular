6b298c437ef2a2ae006f2dc618280fb10f9f558f
Dante <dantearrighi@gmail.com>
2019-04-28 18:35:36 -0300

    New proyect: Forms
    Forms app

---
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent {
+
+  constructor() { }
+
+
+
+guardar(){
+  console.log("Formulario posteado");
+}
+
+
+
+}
