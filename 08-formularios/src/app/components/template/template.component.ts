b4ef9d92952d57298da59a89be24d4b270fcbbda
Dante <dantearrighi@gmail.com>
2019-05-06 15:48:26 -0300

    Fields validations

--- 08-formularios/src/app/components/template/template.component.ts
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
-
+import { NgForm} from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+
+    `]
 })
 export class TemplateComponent {
 
-  constructor() { }
+  usuario: Object = {
+    nombre:null,
+    apellido:null,
+    correo:null
+  };
+  constructor() {
+
+  }
 
 
 
-guardar(){
-  console.log("Formulario posteado");
+guardar( forma:NgForm ){
+
+  console.log("ngForm", forma);
+  console.log("Valor", forma.value);
+
 }
 
 
