f927e33cb9c88df62f30fe7312603e24ee03595a
Dante <dantearrighi@gmail.com>
2019-06-25 17:39:35 -0300

    Chat app
    Firebase Authentication with Google & firebase2
    
    chat.service
     -add message
     -load message
     -login
     -logout

---
+++ 10-firechat/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { ChatComponent } from './components/chat/chat.component';
+
+import { ChatService } from './providers/chat.service';
+import { LoginComponent } from './components/login/login.component';
+
+
+// Componentes
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    FormsModule
+  ],
+  providers: [
+    ChatService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 10-firechat/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCzwWOgWXFkpEJJSlU_rpN_ja-K49YGoC8",
+    authDomain: "firechat-584c7.firebaseapp.com",
+    databaseURL: "https://firechat-584c7.firebaseio.com",
+    projectId: "firechat-584c7",
+    storageBucket: "",
+    messagingSenderId: "565117479838",
+    appId: "1:565117479838:web:c3fa3fce9b7235f7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
